261762109a53755fc4ef7d88f2ae8b23c95e92c8
Ayman Alkorty <al.korty@hotmail.com>
2017-04-07 20:03:59 +0300

    switchstatement

---
+++ switchStatement/switchStatement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           /*  String myAnimal = "cat";
+             switch (myAnimal)
+             {
+                 case "canary":
+                     Console.WriteLine("my animal is the canary birs");
+                     break;
+                 case "cat":
+                     Console.WriteLine("my animal is a cat");
+                     break;
+                 case "camel":
+                     Console.WriteLine("my animal is a camel");
+                     break;
+             }
+             Console.ReadKey();
+             
+            */
+
+
+            int myvar = 20;
+            switch (myvar)
+            {
+                case 15:
+                    Console.WriteLine("my var is 15");
+                    break;
+                case 10:
+                    Console.WriteLine("my var is 10");
+                    break;
+                default:
+                    Console.WriteLine("no one");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
