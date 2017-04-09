15fad1cd14e09e1a0459e56403a8c6b11fbd9c3a
Ayman Alkorty <al.korty@hotmail.com>
2017-04-09 19:08:24 +0300

    implicit_conversion

---
+++ Implicit_conversion/Implicit_conversion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implicit_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long myvar = 1000;
+            float myvar2 = myvar;
+
+            float myfloatvar = 12.5f;
+            int myint2var = myfloatvar;
+
+            //Console.WriteLine($"{myvar2}");
+
+            char myvar3 = 'G';
+            int myintvar = myvar3;
+
+            Console.WriteLine($"{myintvar}");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
